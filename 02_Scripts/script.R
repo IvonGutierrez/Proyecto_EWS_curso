ccaf2e0d06f560aaee39cd26c4742d6eeac2a5f1
IvonGutierrez <ivon.gutierrez@ecologia.unam.mx>
2026-08-18 10:38:14 -0600

    instructions for each line

--- 02_Scripts/script.R
+++ 02_Scripts/script.R
@@ -2,21 +2,23 @@ library(ewsmethods)
 library(dplyr)
 library(tidyr)
 library(tidyverse)
-library(usethis)
+library(usethis)#to set up Git and GitHub
 
 x<-rnorm(1000)
 hist(x)
 #to relate R with GitHub
 
 usethis::gis_git()
-usethis::create_github_token()
-gitcreds::gitcreds_set()
+usethis::create_github_token()#in GitHub creates a Token
+gitcreds::gitcreds_set()##click and copy Token
 usethis::use_git_config(
   user.name = "IvonGutierrez",
   user.email = "ivon.gutierrez@ecologia.unam.mx"
 )
-usethis::use_github()
-usethis::use_github(private = TRUE)
+usethis::use_github()#it creates a repository in GitHub, connect with local repository
+usethis::use_github(private = TRUE)#specify as private##default is public
 
 sd(x)
 
+##for each change, we must use commit. 
+##Check the script, summit the change, then PUSH (select the script)

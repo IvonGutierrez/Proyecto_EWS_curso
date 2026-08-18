f2c1ddda62844b6d80670c3cc93b66ed09b01b49
IvonGutierrez <ivon.gutierrez@ecologia.unam.mx>
2026-08-18 09:06:21 -0600

    last change

--- 02_Scripts/script.R
+++ 02_Scripts/script.R
@@ -2,6 +2,18 @@ library(ewsmethods)
 library(dplyr)
 library(tidyr)
 library(tidyverse)
+library(usethis)
 
 x<-rnorm(1000)
 hist(x)
+#to relate R with GitHub
+
+usethis::gis_git()
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+usethis::use_git_config(
+  user.name = "IvonGutierrez",
+  user.email = "ivon.gutierrez@ecologia.unam.mx"
+)
+usethis::use_github()
+usethis::use_github(private = TRUE)
